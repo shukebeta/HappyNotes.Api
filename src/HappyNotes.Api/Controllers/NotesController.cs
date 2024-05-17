112fba50fec22481c70308d4277941ea79329a97
DW at xps <zhw7@protonmail.ch>
2024-05-17 20:49:58 +1200

    fix inconsistency when return notes result

--- src/HappyNotes.Api/Controllers/NotesController.cs
+++ src/HappyNotes.Api/Controllers/NotesController.cs
@@ -1,18 +1,14 @@
 using Api.Framework;
-using Api.Framework.Extensions;
-using Api.Framework.Helper;
 using Api.Framework.Models;
 using Api.Framework.Result;
 using AutoMapper;
 using HappyNotes.Common;
 using HappyNotes.Dto;
 using HappyNotes.Entities;
-using HappyNotes.Extensions;
 using HappyNotes.Models;
 using HappyNotes.Services.interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.IdentityModel.Tokens;
 
 namespace HappyNotes.Api.Controllers;
 
@@ -39,18 +35,18 @@ public class NotesController(IMapper mapper
 
     [HttpGet("{pageSize:int}/{pageNumber:int}")]
     [EnforcePageSizeLimit(Constants.MaxPageSize)]
-    public async Task<PageData<NoteDto>> MyLatest(int pageSize, int pageNumber)
+    public async Task<ApiResult<PageData<NoteDto>>> MyLatest(int pageSize, int pageNumber)
     {
         var notes = await noteService.MyLatest(pageSize, pageNumber);
-        return notes;
+        return new SuccessfulResult<PageData<NoteDto>>(notes);
     }
 
     [AllowAnonymous]
     [HttpGet("{pageSize:int}/{pageNumber:int}")]
     [EnforcePageSizeLimit(Constants.MaxPageSize)]
-    public async Task<PageData<NoteDto>> Latest(int pageSize, int pageNumber)
+    public async Task<ApiResult<PageData<NoteDto>>> Latest(int pageSize, int pageNumber)
     {
         var notes = await noteService.Latest(pageSize, pageNumber);
-        return notes;
+        return new SuccessfulResult<PageData<NoteDto>>(notes);
     }
 }
