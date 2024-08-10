c77e7166e4268fd4f1237f0fda26e1e219d320b7
DW at xps <weizhong2004@gmail.com>
2024-08-10 13:26:20 +1200

    add TelegramMessageIds field to Note table

---
+++ documents/database/changelist/2024/08-10-add-TelegramMessageIds-field-to-note.sql
@@ -0,0 +1,14 @@
+-- Check if the 'TelegramMessageIds' field exists
+SELECT COUNT(*) INTO @exists FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'HappyNotes'
+  AND TABLE_NAME = 'Note'
+  AND COLUMN_NAME = 'TelegramMessageIds';
+
+-- If the 'TelegramMessageIds' field does not exist, add it
+SET @sql = IF(@exists = 0,
+              'ALTER TABLE `HappyNotes`.`Note` ADD `TelegramMessageIds` VARCHAR(512) NULL COMMENT ''Comma-separated telegram MessageId list'' AFTER `Tags`;',
+              'SELECT "Column TelegramMessageIds already exists." AS Result');
+
+PREPARE stmt FROM @sql;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
