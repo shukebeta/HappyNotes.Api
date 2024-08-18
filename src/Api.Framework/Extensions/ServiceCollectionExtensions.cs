049a85c4c78468680c30510e914a5b06938a4fd4
DW at xps <weizhong2004@gmail.com>
2024-08-18 17:23:40 +1200

    Add Aop logging to ServiceCollectionExtensions; update pars to db.Utilities (97)

--- src/Api.Framework/Extensions/ServiceCollectionExtensions.cs
+++ src/Api.Framework/Extensions/ServiceCollectionExtensions.cs
@@ -23,59 +23,60 @@ public static class ServiceCollectionExtensions
                 DbType = SqlSugar.DbType.MySql, // Required
                 IsAutoCloseConnection = true,
                 InitKeyType = InitKeyType.Attribute
-            });
-
-            client.Aop.OnLogExecuting = (sql, pars) =>
+            }, db =>
             {
-                if (_IsProductionEnv()) return;
-                logger.LogInformation(client.Utilities.SerializeObject(pars.ToDictionary(it => it.ParameterName,
-                    it => it.Value)));
-                foreach (var p in pars)
+                db.Aop.OnLogExecuting = (sql, pars) =>
                 {
-                    string v;
-                    if (p.Value == null)
+                    if (_IsProductionEnv()) return;
+                    logger.LogInformation(db.Utilities.SerializeObject(pars.ToDictionary(it => it.ParameterName,
+                        it => it.Value)));
+                    foreach (var p in pars)
                     {
-                        v = "null";
-                        sql = sql.Replace(p.ParameterName, v);
-                        continue;
+                        string v;
+                        if (p.Value == null)
+                        {
+                            v = "null";
+                            sql = sql.Replace(p.ParameterName, v);
+                            continue;
+                        }
+
+                        switch (p.DbType)
+                        {
+                            case DbType.Byte:
+                            case DbType.SByte:
+                            case DbType.Single:
+                            case DbType.Double:
+                            case DbType.UInt16:
+                            case DbType.UInt32:
+                            case DbType.UInt64:
+                            case DbType.Int16:
+                            case DbType.Int32:
+                            case DbType.Int64:
+                            case DbType.VarNumeric:
+                                v = p.Value!.ToString()!;
+                                break;
+                            case DbType.Boolean:
+                                v = p.Value.Equals(true) ? "true" : "false";
+                                break;
+                            case DbType.DateTime:
+                            case DbType.DateTime2:
+                            case DbType.DateTimeOffset:
+                                v = $"'{((DateTime) p.Value):yyyy-MM-dd HH:mm:ss}'";
+                                break;
+                            default:
+                                string strValue = p.Value?.ToString() ?? "";
+                                if (strValue.Contains(@"'"))
+                                    strValue = Regex.Replace(strValue, @"'", @"\'");
+                                v = $"'{strValue}'";
+                                break;
+                        }
+
+                        sql = Regex.Replace(sql, $@"{p.ParameterName}\b", $"{v}");
                     }
 
-                    switch (p.DbType)
-                    {
-                        case DbType.Byte:
-                        case DbType.SByte:
-                        case DbType.Single:
-                        case DbType.Double:
-                        case DbType.UInt16:
-                        case DbType.UInt32:
-                        case DbType.UInt64:
-                        case DbType.Int16:
-                        case DbType.Int32:
-                        case DbType.Int64:
-                        case DbType.VarNumeric:
-                            v = p.Value!.ToString()!;
-                            break;
-                        case DbType.Boolean:
-                            v = p.Value.Equals(true) ? "true" : "false";
-                            break;
-                        case DbType.DateTime:
-                        case DbType.DateTime2:
-                        case DbType.DateTimeOffset:
-                            v = $"'{((DateTime) p.Value):yyyy-MM-dd HH:mm:ss}'";
-                            break;
-                        default:
-                            string strValue = p.Value?.ToString() ?? "";
-                            if (strValue.Contains(@"'"))
-                                strValue = Regex.Replace(strValue, @"'", @"\'");
-                            v = $"'{strValue}'";
-                            break;
-                    }
-
-                    sql = Regex.Replace(sql, $@"{p.ParameterName}\b", $"{v}");
-                }
-
-                logger.LogInformation(sql);
-            };
+                    logger.LogInformation(sql);
+                };
+            });
 
             return client;
         });
