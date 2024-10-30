559d29dd79503c4c2c177857da9a55176a1140c6
DW at xps <weizhong2004@gmail.com>
2024-10-30 21:25:03 +1300

    Add version information to API responses and deployment workflows

--- src/HappyNotes.Api/Controllers/TestController.cs
+++ src/HappyNotes.Api/Controllers/TestController.cs
@@ -2,6 +2,7 @@
 using Api.Framework.Models;
 using Api.Framework.Result;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
 namespace HappyNotes.Api.Controllers;
@@ -9,22 +10,28 @@ namespace HappyNotes.Api.Controllers;
 /// <summary>
 /// Used for testing only!
 /// </summary>
-public class TestController(ILogger<TestController> logger) : BaseController
+public class TestController(ILogger<TestController> logger, IConfiguration configuration) : BaseController
 {
+    [HttpGet]
+    public string Version()
+    {
+         return $"Version: {configuration["Version"]}";
+     }
+
     [HttpGet]
     public ApiResult<string> ServiceStatus()
     {
         logger.LogInformation("test logger functionality: Information");
         return ResultHelper.New("hello");
     }
- 
+
     [HttpGet]
     public ApiResult SampleFailure()
     {
         logger.LogError("test logger functionality: Error");
         return ResultHelper.New(1, "sample failure");
     }
- 
+
     [HttpGet]
     public ApiResult<JwtToken> SampleCustomException()
     {
