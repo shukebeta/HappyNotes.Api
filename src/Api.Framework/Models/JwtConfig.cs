acfdbeebf097208dc56e92bb4935459783693164
davidwei <davidwei@xps>
2025-10-01 15:28:06 +1300

    Use Api.Framework nuget instead

--- src/Api.Framework/Models/JwtConfig.cs
+++
@@ -1,8 +0,0 @@
-namespace Api.Framework.Models;
-
-public class JwtConfig
-{
-    public string SymmetricSecurityKey { get; init; } = string.Empty;
-
-    public string Issuer { get; init; } = string.Empty;
-}
